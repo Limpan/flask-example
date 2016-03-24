31736cff155b0ac027d2d7e8e417231bafa84243
Linus Törngren <linus@etnolit.se>
2016-03-24 08:45:35 +0100

    Working support for docker containers.

--- web/app/main/views.py
+++ web/app/main/views.py
@@ -1,9 +1,11 @@
 from flask import render_template, url_for, redirect
 from . import main
 from .. import db
+from .models import User, Post
 
 
 @main.route('/')
 def index():
     """Default application route."""
-    return render_template('index.html')
+    users = User.query.all()
+    return render_template('index.html', users=users)
